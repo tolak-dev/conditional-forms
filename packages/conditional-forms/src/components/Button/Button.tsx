4ad9ecd8be4447f3f5fc480525dbcde15d1a66ce
tolak.o <tolak.o@outlook.com>
2025-07-10 19:27:39 +0200

    feat(test): add component

---
+++ packages/conditional-forms/src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface FormButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+}
+
+export const FormButton: React.FC<FormButtonProps> = ({ label, ...props }) => {
+  return <button {...props}>{label} sadsad</button>;
+};
