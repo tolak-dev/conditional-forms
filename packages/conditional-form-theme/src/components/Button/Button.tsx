1f94e6d82a798e1424e175b98dec889fbc1ba2dd
tolak.o <tolak.o@outlook.com>
2025-07-11 22:55:15 +0200

    fix(test): test lllllllllllll

---
+++ packages/conditional-form-theme/src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  label: string;
+}
+
+export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
+  return <button data-testid={`input-button-${label}`}
+ {...props}>{label} TEST sadsad</button>;
+};
