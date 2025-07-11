9bf6ccf89b8caf96ee811f22f660b20b6450245a
tolak.o <tolak.o@outlook.com>
2025-07-11 13:00:29 +0200

    fix: test

--- packages/conditional-forms/src/components/Button/Button.tsx
+++ packages/conditional-forms/src/components/Button/Button.tsx
@@ -6,10 +6,6 @@ export interface FormButtonProps
 }
 
 export const FormButton: React.FC<FormButtonProps> = ({ label, ...props }) => {
-<<<<<<< HEAD
-  return <button {...props}>{label} </button>;
-=======
   return <button {...props}>{label} ttsadsd</button>;
 
->>>>>>> 995bdf8ae457b172a90de1604f2c8f289d4b23f7
 };
