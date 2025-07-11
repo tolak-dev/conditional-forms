71ace6ca4f3fbb09a89bee7e8b616fc630f52935
tolak.o <tolak.o@outlook.com>
2025-07-11 21:21:10 +0200

    fix: test test

--- packages/conditional-forms/src/components/Button/Button.tsx
+++ packages/conditional-forms/src/components/Button/Button.tsx
@@ -7,5 +7,5 @@ export interface ButtonProps
 
 export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
   return <button data-testid={`input-button-${label}`}
- {...props}>{label} TEST  </button>;
+ {...props}>{label} TEST test test </button>;
 };
