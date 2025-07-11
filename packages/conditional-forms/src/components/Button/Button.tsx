ea57fdd62b89086a8a06fd4741658c8177989c8c
tolak.o <tolak.o@outlook.com>
2025-07-11 23:28:49 +0200

    feat(form): update conditional logic for select input

--- packages/conditional-forms/src/components/Button/Button.tsx
+++ packages/conditional-forms/src/components/Button/Button.tsx
@@ -7,5 +7,5 @@ export interface ButtonProps
 
 export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
   return <button data-testid={`input-button-${label}`}
- {...props}>{label} TEST test</button>;
+ {...props}>{label} TEST teasdasdst</button>;
 };
