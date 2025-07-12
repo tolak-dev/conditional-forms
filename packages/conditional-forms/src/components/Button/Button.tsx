7c8a9a71fbc6e932c27a96c29cbbfcf624382a16
tolak.o <tolak.o@outlook.com>
2025-07-12 05:22:49 +0200

    feat(form): aaaaaaaaaaaaaaaabbbccccc

--- packages/conditional-forms/src/components/Button/Button.tsx
+++ packages/conditional-forms/src/components/Button/Button.tsx
@@ -7,5 +7,5 @@ export interface ButtonProps
 
 export const Button: React.FC<ButtonProps> = ({ label, ...props }) => {
   return <button data-testid={`input-button-${label}`}
- {...props}>{label} TEST aaaa </button>;
+ {...props}>{label} TEST bbbbbb </button>;
 };
